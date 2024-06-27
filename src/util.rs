49ed5c3cdc60a7c60804474957c45df9a147233e
4t145 <u4t145@163.com>
2024-06-27 19:04:53 +0800

    make ok mapper const

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,4 @@
-pub fn ok<U, T, E>(map: impl FnOnce(U) -> Result<T, E>) -> impl FnOnce(U) -> Option<T> {
+pub const fn ok<U, T, E>(map: impl FnOnce(U) -> Result<T, E>) -> impl FnOnce(U) -> Option<T> {
     move |u| match map(u) {
         Ok(t) => Some(t),
         Err(_) => None,
